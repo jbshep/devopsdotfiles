66f61400d383821d074ba74448649560e9e41119
jbshep <jbshep@gmail.com>
2025-02-27 10:26:00 -0600

    install.sh script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script can be invoked one of two ways.
+# Either
+#     curl https://raw.githubusercontent.com/jbshep/devopsdotfiles/refs/heads/main/install.sh | bash
+# Or
+#     cd ~
+#     git clone https://github.com/jbshep/devopsdotfiles .dotfiles
+#     cd .dotfiles
+#     ./install.sh link
+
+files=(.vimrc .inputrc)
+
+if [[ -n "$1" && "$1" == "link" ]]; then
+    # Method 2: Git and link
+    for f in ${files[@]}; do 
+        ln -s $f ~/$f
+    done
+else
+    # Method 1: No git
+    for f in ${files[@]}; do 
+        wget https://raw.githubusercontent.com/jbshep/devopsdotfiles/refs/heads/main/$f
+    done
+fi
+
+
+
+
